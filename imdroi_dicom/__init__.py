3ec303ec0192126657c8ce700ab0f57ad9efb856
ChristianWe <christian.weber@mic-services.de>
2018-09-18 14:35:47 +0200

    initial packageing

---
+++ imdroi_dicom/__init__.py
@@ -0,0 +1,1 @@
+name = "imdroi_dicom"
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='ImdroiTools',
+    version='0.1alpha',
+    packages=['imdroi_reporting','imdroi_dicom',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license'
+)
